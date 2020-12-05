274a01816ca30352098a816c1d177052269ac14f
nick <gruzinov15@yandex.ru>
2020-12-05 14:47:40 +0300

    Fix #1 and #2
    - Replace absolute paths
    - Realize that the speed stabilizes way too quickly in `theory.tex`
      using `calculate_when_stable.py` as a calculator
    - Minor update to `.gitignore`

---
+++ calculate_when_stable.py
@@ -0,0 +1,24 @@
+import math
+
+g = 9.82
+rho = 1.12
+eta = 1.2
+mass = 3.9e-4
+diameter = 4e-3
+print(f'Test: density is {mass / (math.pi * diameter**3 / 6)} kg/m^3')
+
+# initial speed
+h = 0.05
+vx0 = math.sqrt(2*g*h)
+
+# stable speed
+c = (mass*g - (1/6)*rho*g*math.pi*(diameter**3)) / (3*math.pi*eta*diameter)
+print(f'c: {c} m/s')
+
+# exponent coefficient
+k = 3*math.pi*eta*diameter / mass
+print(f'k: {k} 1/s')
+
+# time falling until speed stabilizes
+tau = 1/k * math.log((vx0 - c) / (0.01*c))
+print(f'tau: {tau} s')
