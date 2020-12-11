71be31d4c30cfe83519c44d4c302764622daf479
usualhuman <pomayx@gmail.com>
2020-12-11 22:15:57 +0300

    some code formattings

--- processing.py
+++ processing.py
@@ -1,8 +1,9 @@
-import math
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy import stats
 import csv
+import math
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
 
 
 def read_ball_csv(filepath):
@@ -17,56 +18,65 @@ def read_ball_csv(filepath):
     return (np.array(x), np.array(t))
 
 
-def abline(slope, intercept, color = 'b'):
+def abline(slope, intercept, color='b'):
     """Plot a line from slope and intercept"""
     axes = plt.gca()
     x_vals = np.array(axes.get_xlim())
     y_vals = intercept + slope * x_vals
-    plt.plot(x_vals, y_vals, '--', color = color, linewidth = 2)
+    plt.plot(x_vals, y_vals, '--', color=color, linewidth=2)
 
 
 x, t = read_ball_csv('particular_balls/ball_05.csv')
-plt.scatter(t, x); plt.xlabel('time, s'); plt.ylabel('x, cm')
+plt.scatter(t, x)
+plt.xlabel('time, s')
+plt.ylabel('x, cm')
 
 # Add a manually picked line, just to check linearity at the end
 slope = 2.7
 x0, y0 = t[-1], x[-1]
-plt.plot([3, 6.06], [slope*(3 - x0) + y0, slope*(6.06 - x0) + y0], linewidth=3)
+plt.plot([3, 6.06], [slope * (3 - x0) + y0, slope * (6.06 - x0) + y0], linewidth=3)
 
 # Numerical differentiation attempt
 plt.scatter(t, np.gradient(x) / np.gradient(t))
-plt.xlabel('time, s'); plt.ylabel('speed, cm')
+plt.xlabel('time, s')
+plt.ylabel('speed, cm')
 
 # Approximate the tail with linear regression
 tail_start = int(0.8 * len(x))
 model = stats.linregress(t[tail_start:], x[tail_start:])
-plt.scatter(t, x); plt.xlabel('time, s'); plt.ylabel('x, cm')
+plt.scatter(t, x)
+plt.xlabel('time, s')
+plt.ylabel('x, cm')
 abline(model[0], model[1])
 
+
 # Here starts the 10-csvs-long pipeline starts
-def get_speed(x_array, time_array, start_from = 0.8):
+def get_speed(x_array, time_array, start_from=0.8):
     plt.close()
     tail_start = int(start_from * len(x_array))
     time = time_array[tail_start:]
     coord = x_array[tail_start:]
     model = stats.linregress(time, coord)
-    plt.scatter(time_array, x_array); plt.xlabel('time, s'); plt.ylabel('coordinate, cm')
+    plt.scatter(time_array, x_array)
+    plt.xlabel('time, s')
+    plt.ylabel('coordinate, cm')
     abline(model[0], model[1])
     print(model)
-    slope_uncertainty = 2 * math.sqrt(1/len(coord) * (np.std(coord)**2/np.std(time)**2 - model[0]**2))
+    slope_uncertainty = 2 * math.sqrt(1 / len(coord) * (np.std(coord) ** 2 / np.std(time) ** 2 - model[0] ** 2))
     print(f'Slope uncertainty is {slope_uncertainty}')
     return model[0]
 
-#get_speed(*read_ball_csv('particular_balls/ball_01.csv'))
-#get_speed(*read_ball_csv('particular_balls/ball_02.csv'))
-#get_speed(*read_ball_csv('particular_balls/ball_03.csv'))
-#get_speed(*read_ball_csv('particular_balls/ball_04.csv'))
-#get_speed(*read_ball_csv('particular_balls/ball_05.csv'))
-#get_speed(*read_ball_csv('particular_balls/ball_06.csv'))
-#get_speed(*read_ball_csv('particular_balls/ball_07.csv'))
-#get_speed(*read_ball_csv('particular_balls/ball_08.csv'))
-#get_speed(*read_ball_csv('particular_balls/ball_09.csv'))
-#get_speed(*read_ball_csv('particular_balls/ball_10.csv'))
+
+# get_speed(*read_ball_csv('particular_balls/ball_01.csv'))
+# get_speed(*read_ball_csv('particular_balls/ball_02.csv'))
+# get_speed(*read_ball_csv('particular_balls/ball_03.csv'))
+# get_speed(*read_ball_csv('particular_balls/ball_04.csv'))
+# get_speed(*read_ball_csv('particular_balls/ball_05.csv'))
+# get_speed(*read_ball_csv('particular_balls/ball_06.csv'))
+# get_speed(*read_ball_csv('particular_balls/ball_07.csv'))
+# get_speed(*read_ball_csv('particular_balls/ball_08.csv'))
+# get_speed(*read_ball_csv('particular_balls/ball_09.csv'))
+# get_speed(*read_ball_csv('particular_balls/ball_10.csv'))
 
 # List of 10 speeds
 [get_speed(*read_ball_csv(f'particular_balls/ball_0{i}.csv')) for i in range(1, 10)].append(
@@ -79,10 +89,12 @@ speed_uncertainties = [0.03, 0.03, 0.03, 0.02, 0.02, 0.03, 0.02, 0.06, 0.05, 0.0
 diameters, masses = [], []
 with open('balls.csv') as file:
     csv_reader = csv.reader(file, delimiter=',')
-    next(csv_reader); next(csv_reader)
+    next(csv_reader)
+    next(csv_reader)
     for row in csv_reader:
         diameters.append(float(row[1]))
         masses.append(float(row[2]))
+
 
 # Calculate the viscosity from parameters and speed
 def get_viscosity(speed, mass, diameter):
@@ -91,7 +103,8 @@ def get_viscosity(speed, mass, diameter):
     mass /= 1000
     diameter /= 1000
     speed /= 100
-    return g*(mass - rho * np.pi * diameter**3 / 6)/(3 * np.pi * diameter * speed)
+    return g * (mass - rho * np.pi * diameter ** 3 / 6) / (3 * np.pi * diameter * speed)
+
 
 viscosities = []
 for i in range(10):
@@ -112,15 +125,19 @@ x3, t3 = read_ball_csv('particular_balls/ball_07.csv')
 x4, t4 = read_ball_csv('particular_balls/ball_10.csv')
 
 plt.close()
-plt.scatter(t1, x1, label = '1, m ≈ 3.8 г')
-plt.scatter(t2, x2, label = '2, m ≈ 2.4 г')
-plt.scatter(t3, x3, label = '3, m ≈ 3.0 г')
-plt.scatter(t4, x4, label = '4, m ≈ 4.0 г')
-plt.grid(); plt.ylim(ymin=0); plt.xlim(xmin=0)
-plt.legend(fontsize = 14); plt.xlabel('Время, с', fontsize = 18); plt.ylabel('Глубина, см', fontsize = 18)
+plt.scatter(t1, x1, label='1, m ≈ 3.8 г')
+plt.scatter(t2, x2, label='2, m ≈ 2.4 г')
+plt.scatter(t3, x3, label='3, m ≈ 3.0 г')
+plt.scatter(t4, x4, label='4, m ≈ 4.0 г')
+plt.grid()
+plt.ylim(ymin=0)
+plt.xlim(xmin=0)
+plt.legend(fontsize=14)
+plt.xlabel('Время, с', fontsize=18)
+plt.ylabel('Глубина, см', fontsize=18)
 abline(speeds[0], 1.5, 'k')
 abline(speeds[4], 1.75, 'k')
 
 # Get some statistics
 np.mean(viscosities)
-2*np.std(viscosities)
+2 * np.std(viscosities)
